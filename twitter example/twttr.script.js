5a658569284ae67ce7bedfb43f0db456ef5c0d78
hatem.zidi <hatem.zidi@gmail.com>
2013-08-09 21:53:51 +0200

    some checkstyle

--- twitter example/twttr.script.js
+++ twitter example/twttr.script.js
@@ -5,71 +5,92 @@ jQuery(document).ready(function ($) {
 
     $('#tweet-list').hide();
 
-	jQuery.getJSON('../twitter-proxy-local.php?user=' +  user + '&count=' + count , function(tweetdata) {
-				var twtlst = $("#tweet-list");
-				$.each(tweetdata, function(i,tweet) {
-					twtlst.append("<li>&ldquo;" + urlToLink(tweet.text) + "&rdquo;&nbsp;&ndash; " + makeUrl(tweet.id_str,realTime(tweet.created_at)) + "</li>");
-				});
-			});
-	
-	setTimeout(function(){
-		$('.tweets p').hide();
-		$('#tweet-list').show();
-		$('#tweet-list').cycle({
-		//	fx:'scrollDown',
-			speed:500,
-			timeout:3500,
-			pause:1
-		//	cleartypeNoBg:true
-		})
-	},5000);
+    jQuery.getJSON('../twitter-proxy-local.php?user=' + user + '&count=' + count, function (tweetdata) {
+        var twtlst = $("#tweet-list");
+        $.each(tweetdata, function (i, tweet) {
+            twtlst.append("<li>&ldquo;" + urlToLink(tweet.text) + "&rdquo;&nbsp;&ndash; " + makeUrl(tweet.id_str, realTime(tweet.created_at)) + "</li>");
+        });
+    });
+
+    setTimeout(function () {
+        $('.tweets p').hide();
+        $('#tweet-list').show();
+        $('#tweet-list').cycle({
+            //	fx:'scrollDown',
+            speed: 500,
+            timeout: 3500,
+            pause: 1
+            //	cleartypeNoBg:true
+        })
+    }, 5000);
 
 });
 
 //TODO make this user independent
-function makeUrl(id, txt)
-{
-	return '<a href="http://twitter.com/' +  user + '/status/' + id + '" target="_blank">' + txt + '</a>';
+function makeUrl(id, txt) {
+    return '<a href="http://twitter.com/' + user + '/status/' + id + '" target="_blank">' + txt + '</a>';
 }
 
 
-function urlToLink(txt)
-{
-	str = ' '+txt;
-	str = str.replace(/((ftp|https?):\/\/([-\w\.]+)+(:\d+)?(\/([\w/_\.]*(\?\S+)?)?)?)/gm,'<a href="$1" target="_blank">$1</a>');
-	str = str.replace(/([^\w])\@([\w\-]+)/gm,'$1@<a href="http://twitter.com/$2" target="_blank">$2</a>');
-	str = str.replace(/([^\w])\#([\w\-]+)/gm,'$1<a href="http://twitter.com/search?q=%23$2" target="_blank">#$2</a>');
-	return str.replace(/^\s+|\s+$/g, "");
+function urlToLink(txt) {
+    str = ' ' + txt;
+    str = str.replace(/((ftp|https?):\/\/([-\w\.]+)+(:\d+)?(\/([\w/_\.]*(\?\S+)?)?)?)/gm, '<a href="$1" target="_blank">$1</a>');
+    str = str.replace(/([^\w])\@([\w\-]+)/gm, '$1@<a href="http://twitter.com/$2" target="_blank">$2</a>');
+    str = str.replace(/([^\w])\#([\w\-]+)/gm, '$1<a href="http://twitter.com/search?q=%23$2" target="_blank">#$2</a>');
+    return str.replace(/^\s+|\s+$/g, "");
 }
 
-function realTime(pastTime)
-{	
-	var origStamp = Date.parse(pastTime);
-	if (navigator.userAgent.match(/MSIE\s([^;]*)/)) {
+function realTime(pastTime) {
+    var origStamp = Date.parse(pastTime);
+    if (navigator.userAgent.match(/MSIE\s([^;]*)/)) {
         origStamp = Date.parse(pastTime.replace(/( \+)/, ' UTC$1'))
     }
 
-	var curDate = new Date();
-	var currentStamp = curDate.getTime();
-	
-	var difference = parseInt((currentStamp - origStamp)/1000);
+    var curDate = new Date();
+    var currentStamp = curDate.getTime();
 
-	if(difference < 0) return false;
+    var difference = parseInt((currentStamp - origStamp) / 1000);
 
-   	if (difference <= 1) {return "just now";}
-    if (difference < 20) {return difference + " seconds ago";}
-    if (difference < 40) {return "half a minute ago";}
-    if (difference < 60) {return "less than a minute ago";}
-    if (difference <= 90) {return "one minute ago";}
-    if (difference <= 3540) {return Math.round(difference / 60) + " minutes ago";}
-    if (difference <= 5400) {return "1 hour ago";}
-    if (difference <= 86400) {return Math.round(difference / 3600) + " hours ago";}
-    if (difference <= 129600) {return "1 day ago";}
-    if (difference < 604800) {return Math.round(difference / 86400) + " days ago";}
-    if (difference <= 907200) {return "1 week ago";}
-    if (difference <= 2419200) {return Math.round(difference / 604800) + " weeks ago";}
+    if (difference < 0) return false;
 
-	//no calc :/
-	var dateArr = pastTime.split(' ');
-	return dateArr[4].replace(/\:\d+$/,'')+' '+dateArr[2]+' '+dateArr[1]+(dateArr[3]!=curDate.getFullYear()?' '+dateArr[3]:'');
+    if (difference <= 1) {
+        return "just now";
+    }
+    if (difference < 20) {
+        return difference + " seconds ago";
+    }
+    if (difference < 40) {
+        return "half a minute ago";
+    }
+    if (difference < 60) {
+        return "less than a minute ago";
+    }
+    if (difference <= 90) {
+        return "one minute ago";
+    }
+    if (difference <= 3540) {
+        return Math.round(difference / 60) + " minutes ago";
+    }
+    if (difference <= 5400) {
+        return "1 hour ago";
+    }
+    if (difference <= 86400) {
+        return Math.round(difference / 3600) + " hours ago";
+    }
+    if (difference <= 129600) {
+        return "1 day ago";
+    }
+    if (difference < 604800) {
+        return Math.round(difference / 86400) + " days ago";
+    }
+    if (difference <= 907200) {
+        return "1 week ago";
+    }
+    if (difference <= 2419200) {
+        return Math.round(difference / 604800) + " weeks ago";
+    }
+
+    //no calc :/
+    var dateArr = pastTime.split(' ');
+    return dateArr[4].replace(/\:\d+$/, '') + ' ' + dateArr[2] + ' ' + dateArr[1] + (dateArr[3] != curDate.getFullYear() ? ' ' + dateArr[3] : '');
 }
--- twitter-proxy.php
+++ twitter-proxy.php
@@ -2,25 +2,25 @@
 
 /**
  *  Usage:
- *  Send the url you want to access url encoded in the url paramater, for example (This is with JS): 
+ *  Send the url you want to access url encoded in the url paramater, for example (This is with JS):
  *  /twitter-proxy.php?user=tom_z&count=10
  *
  * inspired from : http://www.fullondesign.co.uk/coding/2516-how-use-twitter-oauth-1-1-javascriptjquery.htm
-*/
+ */
 
 // The tokens, keys and secrets from the app you created at https://dev.twitter.com/apps
 $config = array(
-  'oauth_access_token' => 'token-here',
-  'oauth_access_token_secret' => 'token-here',
-  'consumer_key' => 'token-here',
-  'consumer_secret' => 'token-here',
-  'use_whitelist' => true, // If you want to only allow some requests to use this script.
-  'base_url' => 'http://api.twitter.com/1.1/'
+    'oauth_access_token' => 'token-here',
+    'oauth_access_token_secret' => 'token-here',
+    'consumer_key' => 'token-here',
+    'consumer_secret' => 'token-here',
+    'use_whitelist' => true, // If you want to only allow some requests to use this script.
+    'base_url' => 'http://api.twitter.com/1.1/'
 );
 
 // Only allow certain users to twitter. Stop randoms using your server as a proxy.
 $whitelist = array(
-	'tom_z'=>true
+    'tom_z' => true
 );
 
 /*
@@ -29,60 +29,62 @@ $whitelist = array(
 
 // We'll get the user from $_GET[]. 
 // 
-if(!isset($_GET['user'])){
-	die('No user set');
+if (!isset($_GET['user'])) {
+    die('No user set');
 }
 
 $user = $_GET['user'];
 
-if($config['use_whitelist'] && !isset($whitelist[$user])){
-	die('user is not authorised');
+if ($config['use_whitelist'] && !isset($whitelist[$user])) {
+    die('user is not authorised');
 }
 
 // make the url to call
-$url = 'statuses/user_timeline.json?screen_name='. $user .'&count=' . $_GET['count'];
+$url = 'statuses/user_timeline.json?screen_name=' . $user . '&count=' . $_GET['count'];
 
 
 // Figure out the URL parmaters
 $url_parts = parse_url($url);
 parse_str($url_parts['query'], $url_arguments);
 
-$full_url = $config['base_url'].$url; // Url with the query on it.
-$base_url = $config['base_url'].$url_parts['path']; // Url without the query.
+$full_url = $config['base_url'] . $url; // Url with the query on it.
+$base_url = $config['base_url'] . $url_parts['path']; // Url without the query.
 
 /**
-* Code below from http://stackoverflow.com/questions/12916539/simplest-php-example-retrieving-user-timeline-with-twitter-api-version-1-1 by Rivers 
-* with a few modfications by Mike Rogers to support variables in the URL nicely
-*/
+ * Code below from http://stackoverflow.com/questions/12916539/simplest-php-example-retrieving-user-timeline-with-twitter-api-version-1-1 by Rivers
+ * with a few modfications by Mike Rogers to support variables in the URL nicely
+ */
 
-function buildBaseString($baseURI, $method, $params) {
-	$r = array();
-	ksort($params);
-	foreach($params as $key=>$value){
-	$r[] = "$key=" . rawurlencode($value);
-	}
-	return $method."&" . rawurlencode($baseURI) . '&' . rawurlencode(implode('&', $r));
+function buildBaseString($baseURI, $method, $params)
+{
+    $r = array();
+    ksort($params);
+    foreach ($params as $key => $value) {
+        $r[] = "$key=" . rawurlencode($value);
+    }
+    return $method . "&" . rawurlencode($baseURI) . '&' . rawurlencode(implode('&', $r));
 }
 
-function buildAuthorizationHeader($oauth) {
-	$r = 'Authorization: OAuth ';
-	$values = array();
-	foreach($oauth as $key=>$value)
-	$values[] = "$key=\"" . rawurlencode($value) . "\"";
-	$r .= implode(', ', $values);
-	return $r;
+function buildAuthorizationHeader($oauth)
+{
+    $r = 'Authorization: OAuth ';
+    $values = array();
+    foreach ($oauth as $key => $value)
+        $values[] = "$key=\"" . rawurlencode($value) . "\"";
+    $r .= implode(', ', $values);
+    return $r;
 }
 
 // Set up the oauth Authorization array
 $oauth = array(
-	'oauth_consumer_key' => $config['consumer_key'],
-	'oauth_nonce' => time(),
-	'oauth_signature_method' => 'HMAC-SHA1',
-	'oauth_token' => $config['oauth_access_token'],
-	'oauth_timestamp' => time(),
-	'oauth_version' => '1.0'
+    'oauth_consumer_key' => $config['consumer_key'],
+    'oauth_nonce' => time(),
+    'oauth_signature_method' => 'HMAC-SHA1',
+    'oauth_token' => $config['oauth_access_token'],
+    'oauth_timestamp' => time(),
+    'oauth_version' => '1.0'
 );
-	
+
 $base_info = buildBaseString($base_url, 'GET', array_merge($oauth, $url_arguments));
 $composite_key = rawurlencode($config['consumer_secret']) . '&' . rawurlencode($config['oauth_access_token_secret']);
 $oauth_signature = base64_encode(hash_hmac('sha1', $base_info, $composite_key, true));
@@ -90,16 +92,16 @@ $oauth['oauth_signature'] = $oauth_signature;
 
 // Make Requests
 $header = array(
-	buildAuthorizationHeader($oauth), 
-	'Expect:'
+    buildAuthorizationHeader($oauth),
+    'Expect:'
 );
 $options = array(
-	CURLOPT_HTTPHEADER => $header,
-	//CURLOPT_POSTFIELDS => $postfields,
-	CURLOPT_HEADER => false,
-	CURLOPT_URL => $full_url,
-	CURLOPT_RETURNTRANSFER => true,
-	CURLOPT_SSL_VERIFYPEER => false
+    CURLOPT_HTTPHEADER => $header,
+    //CURLOPT_POSTFIELDS => $postfields,
+    CURLOPT_HEADER => false,
+    CURLOPT_URL => $full_url,
+    CURLOPT_RETURNTRANSFER => true,
+    CURLOPT_SSL_VERIFYPEER => false
 );
 
 $feed = curl_init();
@@ -109,9 +111,9 @@ $info = curl_getinfo($feed);
 curl_close($feed);
 
 // Send suitable headers to the end user.
-if(isset($info['content_type']) && isset($info['size_download'])){
-	header('Content-Type: '.$info['content_type']);
-	header('Content-Length: '.$info['size_download']);
+if (isset($info['content_type']) && isset($info['size_download'])) {
+    header('Content-Type: ' . $info['content_type']);
+    header('Content-Length: ' . $info['size_download']);
 
 }
 
